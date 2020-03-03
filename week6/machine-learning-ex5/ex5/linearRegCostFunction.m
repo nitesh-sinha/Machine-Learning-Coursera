4af0eee7e80e52e3aeec27d1ecb7f3297fca92fc
Nitesh Sinha <mit.nitesh@gmail.com>
2020-03-03 08:29:20 +0530

    Add regularized linear regression cost and gradient functions

--- week6/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ week6/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,27 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% Let's calculate the cost
+hypothesis = X * theta;
+errors = hypothesis - y;
+% sumSqErrors = sum(errors .^2);
+sumSqErrors = errors' * errors;
+unregCost = 1/(2*m) * sumSqErrors;
+fprintf("Unreg cost: %f \n", unregCost);
+regularizationCost = lambda/(2*m) * sum(theta(2:end).^2);
+regCost = unregCost + regularizationCost;
+
+% Let's calculate gradient
+unregGrad = 1/m * errors' * X;
+unregGrad = unregGrad(:);
+% Skip regularizing first term
+temp = unregGrad(1);
+regGrad = unregGrad + (lambda/m)*theta;
+regGrad(1) = temp;
 
 
-
-
-
-
-
-
-
-
+J = regCost;
+grad = regGrad;
 
 % =========================================================================
 
