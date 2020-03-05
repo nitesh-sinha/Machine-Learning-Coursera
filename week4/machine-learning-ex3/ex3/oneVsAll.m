fa052c07e20711e7a0fd277c93dffa9cf0823ece
Nitesh Sinha <mit.nitesh@gmail.com>
2020-03-05 15:16:59 +0530

    Describe ONE-VS-ALL multiclass classification process in much more human readable manner

--- week4/machine-learning-ex3/ex3/oneVsAll.m
+++ week4/machine-learning-ex3/ex3/oneVsAll.m
@@ -55,21 +55,29 @@ initial_theta = zeros(n+1,1);
 temp_theta = [];
 options = optimset('GradObj', 'on', 'MaxIter', 100);
 for i = 1:num_labels
-  % Calculate separate optimum theta params for all labels 
+  % Calculate separate optimum theta params for all labels
+  % Note y==i returns a vector of size same as "y" but with 1's at indices 
+  % where y is equal to i; 0's elsewhere
   [optimumTheta] = fmincg(@(t)(lrCostFunction(t, X, (y==i), lambda)), initial_theta, options);
   % fprintf('size of optimum theta: %f %f \n', size(optimumTheta)); % size = [n+1 1]
   temp_theta = [temp_theta optimumTheta]; 
 endfor
 
-% Since temp_theta is of size (n+1) X K, we transpose it to obtain all_theta
-all_theta = temp_theta'; 
+% Since temp_theta is of size "(n+1) X num_labels", we transpose it to obtain all_theta
+all_theta = temp_theta';
 
 
-
-
-
-
-
+% Essentially, in one-vs-all classification problem(assuming K output classes), we do the following:
+%   1. Choose initial theta and train classifier for 1st label being true and rest being false.
+%   2. Choose initial theta and train classifier for 2nd label being true and rest being false.
+%   3. Similarly choose initial thetas and train classifiers for remaining labels being true and rest being false(one at a time).
+%   4. Finally we have K classifier ouputs(optimal theta values), one for each label.
+%   5. Choose 1st classifier output(theta values), use hypothesis equation(maybe g(X*theta)) and calculate output mX1 size hypothesis(say H1).
+%   6. Choose 2nd classifier output(theta values), use hypothesis equation(maybe g(X*theta)) and calculate output mX1 size hypothesis(say H2).
+%   7. Similarly choose all remaining (K-2) classifier outputs, and use hypothesis equation(maybe g(X*theta)) and 
+%           calculate output hypotheses(say H3, H4,....Hk), each of size mX1.
+%   8. Now out of K output hypotheses we have obtained for the first sample, choose the one which is the largest(i.e. probability being highest).
+%   9. Similarly repeat step 8 for all other (m-1) samples.
 % =========================================================================
 
 
