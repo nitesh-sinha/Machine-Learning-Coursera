6e1c37c40ea93c442d02f0188a0beb5840186c70
Nitesh Sinha <mit.nitesh@gmail.com>
2020-03-06 12:01:54 +0530

    Train SVM models using various C and sigma values to calculate optimal params

--- week7/machine-learning-ex6/ex6/dataset3Params.m
+++ week7/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vals = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_vals = [0.01 0.03 0.1 0.3 1 3 10 30];
+% Store model params(C, sigma) and prediction error
+%model_params_and_error = zeros((size(C_vals,2) * size(sigma_vals,2)), 3); 
+model_params_and_error = [];
 
+for i = 1:length(C_vals)
+  for j = 1:length(sigma_vals)
+    model = svmTrain(X, y, C_vals(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vals(j)));
+    predictions = svmPredict(model, Xval);
+    % Calculate prediction error(i.e. fractions of dataset classified incorrectly) on CV set
+    pred_error = mean(double(predictions ~= yval));
+    %fprintf("Using param C = %f and sigma + %f to train SVM. Error is: %f \n", C_vals(i), sigma_vals(j), pred_error);
+    model_params_and_error = [model_params_and_error; [C_vals(i) sigma_vals(j) pred_error]];
+  endfor
+endfor
 
-
-
-
+% Calculate lowest prediction error and corresponding model params
+[min_error indx] = min(model_params_and_error(:,3));
+%fprintf("Min prediction error on CV set is: %f \n. Corresponding model params are: C= %f and sigma = %f", 
+%        min_error, model_params_and_error(indx, 1), model_params_and_error(indx, 2));
+        
+C = model_params_and_error(indx, 1);
+sigma = model_params_and_error(indx, 2);
 
 % =========================================================================
 
--- week7/machine-learning-ex6/ex6/svmPredict.m
+++ week7/machine-learning-ex6/ex6/svmPredict.m
@@ -26,7 +26,7 @@ if strcmp(func2str(model.kernelFunction), 'linearKernel')
 elseif strfind(func2str(model.kernelFunction), 'gaussianKernel')
     % Vectorized RBF Kernel
     % This is equivalent to computing the kernel on every pair of examples
-    X1 = sum(X.^2, 2);
+    X1 = sum(X.^2, 2); % Sum elements along the row
     X2 = sum(model.X.^2, 2)';
     K = bsxfun(@plus, X1, bsxfun(@plus, X2, - 2 * X * model.X'));
     K = model.kernelFunction(1, 0) .^ K;
